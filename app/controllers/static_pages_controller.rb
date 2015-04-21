536e85c69ce34748ef4f756fef03cf4ccaf7ed73
Jeff Wolf <jeffcwolf@gmail.com>
2015-04-21 17:23:44 -0500

    Implements new API calls using proper formatting

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,7 +4,37 @@ require 'json'
 class StaticPagesController < ApplicationController
   def home
 
-    @booksapi_url = "http://api.nytimes.com/svc/books/v3/lists/travel.json?api-key=#{NYT_BOOKSAPI_KEY}"
+    #Obtain book list names for subsequent use in API requests
+
+    @listnames_url = "http://api.nytimes.com/svc/books/v3/lists/names.json?api-key=#{NYT_BOOKSAPI_KEY}"
+
+    @raw_results_list = open(@listnames_url).read
+
+    @parsed_results_list = JSON.parse(@raw_results_list)
+
+    @listnames_full = @parsed_results_list["results"]
+
+    #Take full list and iterate through only the list names in each array
+    #Save those names in a new array
+
+    @listnames = []
+
+    @listnames_full.each do |list|
+      @listnames.push(list["list_name"])
+    end
+
+    #Replace spaces with hyphens and downcase (e.g. hardcover-fiction)
+    #to obtain URL-ready list names
+
+    @listnames_URL_ready = []
+
+    @listnames.each do |list_item|
+      @listnames_URL_ready.push(list_item.downcase.gsub(/\s/, '-'))
+    end
+
+    #Implement the API request for the list of Best Sellers by Genre
+
+    @booksapi_url = "http://api.nytimes.com/svc/books/v3/lists/#{@listnames_URL_ready[3]}.json?api-key=#{NYT_BOOKSAPI_KEY}"
 
     @raw_results = open(@booksapi_url).read
 
