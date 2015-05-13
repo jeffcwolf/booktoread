004dc1542951b91ab018e34b28811c4bf0173699
Jeff Wolf <jeffcwolf@gmail.com>
2015-05-13 09:11:23 -0500

    Modifies jquery commands

---
+++ app/assets/javascripts/myapp.js
@@ -0,0 +1,10 @@
+// Include my jQuery in this file
+
+$(function (){
+    var backToTop = "<a href='#top'>Back to Top</a>";
+    $("h6").css('border','3px solid #CCF');
+    var loadGenre = "<a href="
+    $("div.booklist").css('border','3px solid #CFC');
+    $("div.bookitem").css('border','3px solid #FCC');
+    $("div.bookitem").append(backToTop);
+});
